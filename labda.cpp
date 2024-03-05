60a244d65ac01dbf952667183d826c0abf3b43eb
Allan Hanauer <allan.hanauer.dev@gmail.com>
2024-03-05 02:53:10 -0300

    INITIAL COMMIT

--- labda.cpp
+++ labda.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <typeinfo> // Para RTTI
+#include <functional> // Para std::function
 
-class Shape {
-public:
-    virtual double area() const = 0; // Função virtual pura para calcular a área
-};
-
-class Circle : public Shape {
-private:
-    double radius;
-
-public:
-    Circle(double r) : radius(r) {}
-
-    double area() const override {
-        return 3.14159 * radius * radius; // Área do círculo: π * raio^2
-    }
-};
-
-class Square : public Shape {
-private:
-    double side;
-
-public:
-    Square(double s) : side(s) {}
-
-    double area() const override {
-        return side * side; // Área do quadrado: lado^2
-    }
-};
-
+using namespace std;
 int main() {
-    std::vector<Shape*> shapes;
-    shapes.push_back(new Circle(3.0)); // Adicionando um círculo à lista
-    shapes.push_back(new Square(4.0)); // Adicionando um quadrado à lista
-
-    // Iterando sobre a lista de formas e calculando a área de cada uma
-    for (Shape* shape : shapes) {
-        if (Circle* circle = dynamic_cast<Circle*>(shape)) {
-            std::cout << "Area do circulo: " << circle->area() << std::endl;
-        } else if (Square* square = dynamic_cast<Square*>(shape)) {
-            std::cout << "Area do quadrado: " << square->area() << std::endl;
+    // Definindo a lambda para realizar operações
+    typedef function<double(double, double, char)> Operation;
+    Operation operacao = [](double a, double b, char op) -> double {
+        switch(op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b != 0) {
+                    return a / b;
+                } else {
+                    cout << "Erro: Divisão por zero!" << endl;
+                    return 0;
+                }
+            default:
+                cout << "Operação inválida!" << endl;
+                return 0;
         }
-    }
+    };
 
-    // Liberando a memória alocada
-    for (Shape* shape : shapes) {
-        delete shape;
-    }
+    // Testando a lambda com diferentes operações
+    cout << "3 + 4 = " << operacao(3, 4, '+') << endl;
+    cout << "7 - 2 = " << operacao(7, 2, '-') << endl;
+    cout << "5 * 6 = " << operacao(5, 6, '*') << endl;
+    cout << "8 / 2 = " << operacao(8, 2, '/') << endl;
+    cout << "9 / 0 = " << operacao(9, 0, '/') << endl; // Tentativa de divisão por zero
 
     return 0;
 }
