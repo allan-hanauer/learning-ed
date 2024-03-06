5bc7636ad131891e300edd8e6b6defca4134eeea
Allan Hanauer <allan.hanauer.dev@gmail.com>
2024-03-06 02:12:30 -0300

    hash

--- hashTable.cpp
+++ hashTable.cpp
@@ -1,37 +1,77 @@
 #include <iostream>
 using namespace std;
-struct Value{
+
+struct Value {
     int price;
 };
-struct Node{
+
+struct Node {
     string key;
-    Value* price=nullptr;
-    Node* next=nullptr;
+    Value* price = nullptr;
+    Node* next = nullptr;
 };
 
-struct HashTable{
-    Node* First=nullptr;
-    void insertKey(string key){
-        Node* newNode = new Node();
-        newNode->key = key;
-        if(First == nullptr){
-            cout << "Sucesso" << endl;
-            First = newNode;
-        }else{
+struct HashTable {
+    Node* First = nullptr;
+
+    void insertKey(string key) {
         Node* current = First;
-        while(current->next != nullptr){
+        while (current != nullptr) {
+            if (current->key == key) {
+                return;
+            }
             current = current->next;
         }
-        current->next = newNode;
+
+        Node* newNode = new Node();
+        newNode->key = key;
+        if (First == nullptr) {
+            First = newNode;
+        } else {
+            current = First;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
         }
+    }
+
+    void insertValue(string key, int value) {
+        Value* newValue = new Value();
+        newValue->price = value;
+        Node* current = First;
+        while (current != nullptr) {
+            if (current->key == key) {
+                current->price = newValue;
+                return;
+            } else {
+                current = current->next;
+            }
+        }
+        return;
+    }
+
+    void print() {
+        Node* current = First;
+        cout << "{" << endl;
+        while (current != nullptr) {
+            if (current->price != nullptr) {
+                cout << current->key << " : " << current->price->price << ", " << endl;
+            }
+            current = current->next;
+        }
+        cout << "}" << endl;
     }
 };
 
-int main(void){
+int main(void) {
     HashTable a1;
     a1.insertKey("Banana");
-    a1.insertKey("Maça");
-    cout << a1.First->key << endl;
-    cout << a1.First->next->key;
+    a1.insertKey("Maca");
+    a1.insertValue("Banana", 10);
+    a1.insertKey("Morango");
+    a1.insertValue("Morango", 10);
+    a1.insertValue("Maca", 20);
+    a1.print();
     return 0;
 }
